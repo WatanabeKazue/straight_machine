c1f148f64433d99c6c95e1669cdd94f8176fd9bd
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-05-08 10:50:20 +0900

    add uekawa uetea team

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,6 +9,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+// String _text01 ="おめでとうございます";
+// String _text02 ="合格です";
+// String _text03 ="よくできました";
+// String _text04 ="不合格です";
+// String _text05 ="まあまあかな";
+// String _text06 ="やり直し";
+  List<String> _texts = ["おめでとうございます","合格です","よくできました","不合格です",
+     "まあまあかな","やり直し"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,38 +26,54 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("ツッコミマシーン"),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                _soundButton(),
-                _soundButton(),
-              ],
-            ),
-          ), 
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                _soundButton(),
-                _soundButton(),
-              ],
-            ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(flex: 1, child: _soundButton(_texts[0])),
+                    Expanded(flex: 1, child: _soundButton(_texts[1])),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(flex: 1, child: _soundButton(_texts[2])),
+                    Expanded(flex: 1, child: _soundButton(_texts[3])),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(flex: 1, child: _soundButton(_texts[4])),
+                    Expanded(flex: 1, child: _soundButton(_texts[5])),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                _soundButton(),
-                _soundButton(),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
 
-  Widget _soundButton() {
-    return ElevatedButton();
+  Widget _soundButton(String displayText) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        child: Text( displayText),
+      ),
+    );
   }
 }
