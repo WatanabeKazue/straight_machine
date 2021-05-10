fafb0c9352ed64d269c6c9d5d0680652629bd66d
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-05-10 12:39:17 +0900

    finish 107

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:ffi';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:soundpool/soundpool.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,15 +11,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<String> _texts = [
+    "おめでとうございます",
+    "合格です",
+    "よくできました",
+    "残念でした",
+    "不合格です",
+    "頑張りましょう"
+  ];
 
-// String _text01 ="おめでとうございます";
-// String _text02 ="合格です";
-// String _text03 ="よくできました";
-// String _text04 ="不合格です";
-// String _text05 ="まあまあかな";
-// String _text06 ="やり直し";
-  List<String> _texts = ["おめでとうございます","合格です","よくできました","不合格です",
-     "まあまあかな","やり直し"];
+  List<int> _soundIds = [0, 0, 0, 0, 0, 0];
+
+  // List<int> soundld = List();
+
+  Soundpool _soundpool;
+
+  int number = 3;
+
+  @override
+  void initState() {
+    _initSounds();
+    super.initState();
+  }
+
+  void _initSounds() async {
+    _soundpool = Soundpool();
+
+    _soundIds[0] = await loadSound("assets/sounds/sound1.mp3");
+    // _soundIds[0] = await rootBundle
+    //     .load("assets/sounds/sound1.mp3")
+    //     .then((value) => _soundpool.load(value));
+  }
+
+  Future<int> loadSound(String s) {
+    return null;
+  }
+
+  @override
+  void dispose() {
+    _soundpool.release();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,12 +102,16 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _soundButton(String displayText) {
+  Widget _soundButton(String displayText,) {
     return Container(
       padding: EdgeInsets.all(8.0),
       child: ElevatedButton(
-        child: Text( displayText),
+        onPressed: null, //Todo 音を鳴らす
+        child: Text(displayText),
       ),
     );
   }
 }
+// _soundIds[0] = await rootBundle
+//     .load("assets/sounds/sound1.mp3")
+//     .then((Value)=>_soundpool.load(value));
